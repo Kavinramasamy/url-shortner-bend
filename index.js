07f9255bef72dae67c491b0aceba1abe878b84f0
Kavinramasamy <kavinramasamymech@gmail.com>
2023-11-21 21:59:02 +0530

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { db_connection } from './DataBase/DB.js';
 import { } from "dotenv/config.js";
+import { RouterPage } from './Routers/RouterPage.js';
 const app = express();
 
 
@@ -10,7 +11,6 @@ db_connection();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-// app.use('/', RouterPage);
-app.get("/", (req, res) => { res.status(200).json({ message: "Working fine..." }) })
+app.use('/', RouterPage);
 
 app.listen(process.env.PORT, () => console.log("Server connected", process.env.PORT));
